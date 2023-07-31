edaaf0025643ca0109c4d0febb963799145ce485
ethan-assefa <affesa.ethan@gmail.com>
2023-07-31 10:10:48 -0400

    Updated URL for GitHub

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 setup(
     name='BookLove',
     version='1.0.0',
-    url='https://github.com/mypackage.git',
+    url='https://github.com/ethan-assefa/DS5100_booklover.git',
     author='Ethan Assefa',
     author_email='eda8ek@virginia.com',
     description='A package that wraps up the BookLover class and the test suite for it. (This is work for M09 of the DS5100 course at UVA SDS)',
